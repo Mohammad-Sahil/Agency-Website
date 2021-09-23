8156cbb457e7db9ab4b22aaa47dc6810c5be24a0
Mohammad Sahil <sahilmejakhas@gmail.com>
2021-09-23 12:20:15 +0530

    Updates

---
+++ precache-manifest.d15d4bdab47bbce838c003cc435e2cb2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fe004adda91bb3409fe879ca442f03",
+    "url": "/Agency-Website/index.html"
+  },
+  {
+    "revision": "b0e173c749e20424b9df",
+    "url": "/Agency-Website/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "01ac3b58c5f4e1c68683",
+    "url": "/Agency-Website/static/js/2.a92f600b.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/Agency-Website/static/js/2.a92f600b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e173c749e20424b9df",
+    "url": "/Agency-Website/static/js/main.3d2457ca.chunk.js"
+  },
+  {
+    "revision": "e7b144adcb3ad341a3d8",
+    "url": "/Agency-Website/static/js/runtime-main.1bee0579.js"
+  },
+  {
+    "revision": "eda4598198a00832fb334d5a4e588ead",
+    "url": "/Agency-Website/static/media/01-thumbnail.eda45981.jpg"
+  },
+  {
+    "revision": "e52270b3af9b353d02078b01acd931ae",
+    "url": "/Agency-Website/static/media/02-thumbnail.e52270b3.jpg"
+  },
+  {
+    "revision": "09c41872ba6c96c6efdcab7f202a71a7",
+    "url": "/Agency-Website/static/media/03-thumbnail.09c41872.jpg"
+  },
+  {
+    "revision": "80c8ff5c4a1a0775616a5541086b1d51",
+    "url": "/Agency-Website/static/media/04-thumbnail.80c8ff5c.jpg"
+  },
+  {
+    "revision": "68d8d9495f59fa5109f3b337d784cacc",
+    "url": "/Agency-Website/static/media/05-thumbnail.68d8d949.jpg"
+  },
+  {
+    "revision": "20456fff3658b40a59a73ae9821a273f",
+    "url": "/Agency-Website/static/media/06-thumbnail.20456fff.jpg"
+  },
+  {
+    "revision": "5a85dd0ce09ff48dcbc9744f08b9b42e",
+    "url": "/Agency-Website/static/media/1.5a85dd0c.png"
+  },
+  {
+    "revision": "a90b0f86f471b7840c8c3b678620cbf3",
+    "url": "/Agency-Website/static/media/2.a90b0f86.jpg"
+  },
+  {
+    "revision": "cc41e47b8fbdb1c81c375b65071e478c",
+    "url": "/Agency-Website/static/media/2.cc41e47b.jpg"
+  },
+  {
+    "revision": "2df43e3546ec0d6666badb2ddbc716ee",
+    "url": "/Agency-Website/static/media/3.2df43e35.jpg"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/Agency-Website/static/media/header-bg.e2d2a51c.jpg"
+  }
+]);
